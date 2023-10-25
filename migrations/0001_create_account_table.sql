11692df451261c40b73f00a44d310e1d9b1f9cf0
KuengNoel <74761380+KuengNoel@users.noreply.github.com>
2023-10-25 14:24:52 +0200

    7 implement apiuserupdate + tests (#22)
    * Implemented authentication function + tests + fixed minor imperfections (#16) (#17)
    
    * Update main.rs
    
    * Implemented update function for account
    
    * fixed minor imperfections
    
    * Added insomnia tests
    
    * Update insomnia_auth_service.json

--- migrations/0001_create_account_table.sql
+++ migrations/0001_create_account_table.sql
@@ -8,9 +8,9 @@
  *******************************************************/
 
 CREATE TABLE account (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    write_key VARCHAR(255) NOT NULL,
-    read_key VARCHAR(255) NOT NULL
+    id          SERIAL PRIMARY KEY,
+    email       VARCHAR(255) UNIQUE NOT NULL,
+    password    VARCHAR(255) NOT NULL,
+    write_key   VARCHAR(255) UNIQUE NOT NULL,
+    read_key    VARCHAR(255) UNIQUE NOT NULL
 );
--- src/service/account_service.rs
+++ src/service/account_service.rs
@@ -8,7 +8,13 @@
  *******************************************************/
 
 use actix_web::web::Data;
+
+use bcrypt::DEFAULT_COST;
+use bcrypt::hash;
+
 use sqlx::PgPool;
+use sqlx::postgres::PgQueryResult;
+
 use crate::entity::AccountEntity;
 
 pub struct AccountService;
@@ -16,8 +22,17 @@ pub struct AccountService;
 impl AccountService {
     pub async fn find_by_email(pool: &Data<PgPool>, email: &String) -> crate::Result<Option<AccountEntity>> {
         Ok(
-            sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE email = $1"#)
+            sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE email = $1;"#)
             .bind(email)
+            .fetch_optional(&***pool)
+            .await?
+        )
+    }
+
+    pub async fn find_by_write_key(pool: &Data<PgPool>, write_key: &String) -> crate::Result<Option<AccountEntity>> {
+        Ok(
+            sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE write_key = $1;"#)
+            .bind(write_key)
             .fetch_optional(&***pool)
             .await?
         )
@@ -25,14 +40,14 @@ impl AccountService {
 
     pub async fn find_by_read_key(pool: &Data<PgPool>, read_key: &String) -> crate::Result<Option<AccountEntity>> {
         Ok(
-            sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE read_key = $1"#)
+            sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE read_key = $1;"#)
             .bind(read_key)
             .fetch_optional(&***pool)
             .await?
         )
     }
 
-    pub async fn create(pool: Data<PgPool>, email: &String, password: &String) -> crate::Result<AccountEntity> {
+    pub async fn create(pool: &Data<PgPool>, email: &String, password: &String) -> crate::Result<AccountEntity> {
         let entity = AccountEntity::new(email, password)?;
         
         Ok(
@@ -41,8 +56,20 @@ impl AccountService {
             .bind(&entity.password)
             .bind(&entity.write_key)
             .bind(&entity.read_key)
-            .fetch_one(&**pool)
+            .fetch_one(&***pool)
             .await?
+        )
+    }
+
+    pub async fn update(pool: &Data<PgPool>, write_key: &String, email: &String, password: &String) -> crate::Result<PgQueryResult> {
+        let password = hash(password, DEFAULT_COST)?;
+        
+        Ok(
+            sqlx::query(r#"UPDATE account SET email = $1, password = $2 WHERE write_key = $3;"#)
+            .bind(email)
+            .bind(password)
+            .bind(write_key)
+            .execute(&***pool).await?
         )
     }
 }
