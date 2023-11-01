d6056dd004e409962111e3c6e0218a4e6e36ace0
KuengNoel <74761380+KuengNoel@users.noreply.github.com>
2023-11-01 13:55:20 +0100

    Lock table for multiple transaction (#33)

--- src/service/account_service.rs
+++ src/service/account_service.rs
@@ -7,49 +7,48 @@
  * permission of an Blackwood Studio Admin
  *******************************************************/
 
-use actix_web::web::Data;
-
 use bcrypt::DEFAULT_COST;
 use bcrypt::hash;
 
-use sqlx::PgPool;
+use sqlx::Postgres;
+use sqlx::Transaction;
 
 use uuid::Uuid;
 
 use crate::Error;
-use crate::entity::AccountEntity;
+use crate::entity::AccountEntity; 
 
 pub struct AccountService;
 
 impl AccountService {
-    pub async fn find_by_email(pool: &Data<PgPool>, email: &String) -> Result<Option<AccountEntity>, Error> {
+    pub async fn find_by_email(transaction: &mut Transaction<'_, Postgres>, email: &String) -> Result<Option<AccountEntity>, Error> {
         Ok(
             sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE email = $1;"#)
             .bind(email)
-            .fetch_optional(&***pool)
+            .fetch_optional(&mut **transaction)
             .await?
         )
     }
 
-    pub async fn find_by_write_key(pool: &Data<PgPool>, write_key: &String) -> Result<Option<AccountEntity>, Error> {
+    pub async fn find_by_write_key(transaction: &mut Transaction<'_, Postgres>, write_key: &String) -> Result<Option<AccountEntity>, Error> {
         Ok(
             sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE write_key = $1;"#)
             .bind(write_key)
-            .fetch_optional(&***pool)
+            .fetch_optional(&mut **transaction)
             .await?
         )
     }
 
-    pub async fn find_by_read_key(pool: &Data<PgPool>, read_key: &String) -> Result<Option<AccountEntity>, Error> {
+    pub async fn find_by_read_key(transaction: &mut Transaction<'_, Postgres>, read_key: &String) -> Result<Option<AccountEntity>, Error> {
         Ok(
             sqlx::query_as::<_, AccountEntity>(r#"SELECT * FROM account WHERE read_key = $1;"#)
             .bind(read_key)
-            .fetch_optional(&***pool)
+            .fetch_optional(&mut **transaction)
             .await?
         )
     }
 
-    pub async fn create(pool: &Data<PgPool>, email: &String, password: &String) -> Result<AccountEntity, Error> {
+    pub async fn create(transaction: &mut Transaction<'_, Postgres>, email: &String, password: &String) -> Result<AccountEntity, Error> {
         let password_hash = hash(password, DEFAULT_COST)?;
         let write_key = Uuid::new_v4().to_string();
         let read_key = Uuid::new_v4().to_string();
@@ -60,17 +59,18 @@ impl AccountService {
             .bind(password_hash)
             .bind(write_key)
             .bind(read_key)
-            .fetch_one(&***pool)
+            .fetch_one(&mut **transaction)
             .await?
         )
     }
 
-    pub async fn update(pool: &Data<PgPool>, write_key: &String, email: &Option<String>, password: &Option<String>) -> Result<(), Error> {
+    pub async fn update(transaction: &mut Transaction<'_, Postgres>, write_key: &String, email: &Option<String>, password: &Option<String>) -> Result<(), Error> {
         if let Some(email) = email {
             sqlx::query(r#"UPDATE account SET email = $1 WHERE write_key = $2;"#)
             .bind(email)
             .bind(write_key)
-            .execute(&***pool).await?;
+            .execute(&mut **transaction)
+            .await?;
         }
 
         if let Some(password) = password {
@@ -79,16 +79,18 @@ impl AccountService {
             sqlx::query(r#"UPDATE account SET password_hash = $1 WHERE write_key = $2;"#)
             .bind(password_hash)
             .bind(write_key)
-            .execute(&***pool).await?;
+            .execute(&mut **transaction)
+            .await?;
         }
 
         Ok(())
     }
 
-    pub async fn delete(pool: &Data<PgPool>, write_key: &String) -> Result<(), Error> {
+    pub async fn delete(transaction: &mut Transaction<'_, Postgres>, write_key: &String) -> Result<(), Error> {
         sqlx::query(r#"DELETE FROM account WHERE write_key = $1;"#)
         .bind(write_key)
-        .execute(&***pool).await?;
+        .execute(&mut **transaction)
+        .await?;
 
         Ok(())
     }
