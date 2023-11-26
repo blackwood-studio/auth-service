dfcdeacb256b4ffce8b4f0d8e21311ce3037c0d1
KuengNoel <noel.kueng@ik.me>
2023-11-26 03:40:42 +0100

    Implemented pool into service

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -6,11 +6,6 @@
  * The Auth Project can not be copied and/or distributed without the express
  * permission of an Blackwood Studio Admin
  *******************************************************/
-
-mod error;
-pub use error::Error;
-
-pub mod dto;
-pub mod entity;
-pub mod service;
-pub mod transaction;
+ 
+mod account_transaction;
+pub use account_transaction::AccountTransaction;
